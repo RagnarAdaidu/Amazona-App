050fc551df5a1f31b19a7c50f95b40a185f95dbd
Eten-Ekata <noeladaidu@gmail.com>
2023-05-02 10:00:45 +0100

    share code on github

---
+++ src/data.js
@@ -0,0 +1,72 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'Putin Badman Pants',
+      category: 'Shirts',
+      image: 'images/pants1.jpg ',
+      price: 430,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality product'
+    },
+    {
+      _id: '2',
+      name: 'Chicjs Uzoh Pants',
+      category: 'Shirts',
+      image: 'images/pants2.jpg ',
+      price: 76,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality product'
+    },
+    {
+      _id: '3',
+      name: 'Nike Slim Pants',
+      category: 'Shirts',
+      image: 'images/pants3.jpg ',
+      price: 120,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality product'
+    },
+    {
+      _id: '4',
+      name: 'Dan Kaduna Shirt',
+      category: 'Shirts',
+      image: 'images/shirt1.jpg ',
+      price: 543,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality product'
+    },
+    {
+      _id: '5',
+      name: 'Aba myBeloved Shirt',
+      category: 'Shirts',
+      image: 'images/shirt2.jpg ',
+      price: 90,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality product'
+    },
+    {
+      _id: '6',
+      name: 'Adidas Slim Shirt',
+      category: 'Shirts',
+      image: 'images/shirt3.jpg ',
+      price: 123,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality product'
+    }
+  ]
+}
+
+export default data
